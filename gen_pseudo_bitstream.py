832bc0b1e636f408fe6066e5d579e083acf90543
osboxes <osboxes@vtrmachine.myguest.virtualbox.org>
2025-08-05 22:38:19 -0500

    still working on python file

--- gen_pseudo_bitstream.py
+++ gen_pseudo_bitstream.py
@@ -1,2 +1,9 @@
 if __name__ == "__main__":
-    print("hello")
+    net_file_path = "rudra_testing_simple/temp/fulladd.net.post_routing"
+    fasm_file_path = "rudra_testing_simple/temp/fulladd.fasm"
+    place_file_path = "rudra_testing_simple/temp/fulladd.place"
+    route_file_path = rudra_testing_simple/temp/fulladd.route"
+
+    net_file = open(net_file_path)
+
+    
